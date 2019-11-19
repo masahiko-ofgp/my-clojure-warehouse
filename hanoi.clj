12664cc30cdb8b8240c45f9fbb42ce7fe0ae4dcc
masahiko-ofgp <ichigyo.zanmai@gmail.com>
2019-11-19 15:46:44 +0900

    Modified hanoi.clj

--- hanoi.clj
+++ hanoi.clj
@@ -1,9 +1,8 @@
 (defn hanoi [n a b c]
   (when (pos? n)
-    (do
-      (hanoi (dec n) a c b)
-      (println a "=>" c)
-      (recur (dec n) b a c))))
+    (hanoi (dec n) a c b)
+    (println a "=>" c)
+    (recur (dec n) b a c)))
 
 (defn run-hanoi [disks]
   (hanoi disks \A \B \C))
